be1db1a297c72501fafe8daf1910d2f14c248dd7
zhaodawei <zhaodawei@mobike.com>
2016-07-29 10:25:56 +0800

    no message

--- LoadROM.cs
+++ LoadROM.cs
@@ -26,6 +26,17 @@ namespace Multi_IAP_Application
         public string ROMLB_3_4_Ver;
 
 
+        int ROMLB_3_3_Bin_Size;
+        BinaryReader ROMLB_3_3_bin_read;
+        byte[] ROMLB_3_3_bytes;
+
+
+        int ROMLB_3_4_Bin_Size;
+        BinaryReader ROMLB_3_4_bin_read;
+        byte[] ROMLB_3_4_bytes;
+
+
+
         public LoadROM()
         {
             InitializeComponent();
@@ -40,6 +51,34 @@ namespace Multi_IAP_Application
 
             listView1.Items[0].SubItems[1].Text = Path.GetFileName(ROMLB3_3_Path);
             listView1.Items[1].SubItems[1].Text = Path.GetFileName(ROMLB3_4_Path);
+
+            if (ROMLB3_3_Path != "")
+            {
+                FileInfo info1 = new FileInfo(ROMLB3_3_Path);
+                int size1 = (int)info1.Length;
+                ROMLB_3_3_Bin_Size = size1;
+                FileStream fs1 = new FileStream(ROMLB3_3_Path, FileMode.Open, FileAccess.Read);
+                ROMLB_3_3_bin_read = new BinaryReader(fs1);
+                ROMLB_3_3_bytes = new byte[ROMLB_3_3_Bin_Size];
+                ROMLB_3_3_bytes = ROMLB_3_3_bin_read.ReadBytes(ROMLB_3_3_Bin_Size);
+                ROMLB_3_3_bin_read.Close();
+                fs1.Close();
+            }
+
+            if (ROMLB3_4_Path != "")
+            {
+                FileInfo info2 = new FileInfo(ROMLB3_4_Path);
+                int size2 = (int)info2.Length;
+                ROMLB_3_4_Bin_Size = size2;
+                FileStream fs2 = new FileStream(ROMLB3_4_Path, FileMode.Open, FileAccess.Read);
+                ROMLB_3_4_bin_read = new BinaryReader(fs2);
+                ROMLB_3_4_bytes = new byte[ROMLB_3_4_Bin_Size];
+                ROMLB_3_4_bytes = ROMLB_3_4_bin_read.ReadBytes(ROMLB_3_4_Bin_Size);
+                ROMLB_3_4_bin_read.Close();
+                fs2.Close();
+            }
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -53,14 +92,27 @@ namespace Multi_IAP_Application
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
 
-                string strFileName = openFileDialog.FileName;
-                listView1.Items[0].SubItems[1].Text = Path.GetFileName(strFileName);
+                ROMLB3_3_Path = openFileDialog.FileName;
+                listView1.Items[0].SubItems[1].Text = Path.GetFileName(ROMLB3_3_Path);
 
-                string tPath = Properties.Settings.Default.LB3_3_FilePath;
-            
-                textBox1.Text = openFileDialog.FileName;
-                Properties.Settings.Default.LB3_3_FilePath = textBox1.Text;
+                textBox1.Text = ROMLB3_3_Path;
+                Properties.Settings.Default.LB3_3_FilePath = ROMLB3_3_Path;
                 Properties.Settings.Default.Save();  // save 文件路径
+
+                if (ROMLB3_3_Path != "")
+                {
+                    FileInfo info1 = new FileInfo(ROMLB3_3_Path);
+                    int size1 = (int)info1.Length;
+                    ROMLB_3_3_Bin_Size = size1;
+                    FileStream fs1 = new FileStream(ROMLB3_3_Path, FileMode.Open, FileAccess.Read);
+                    ROMLB_3_3_bin_read = new BinaryReader(fs1);
+                    ROMLB_3_3_bytes = new byte[ROMLB_3_3_Bin_Size];
+                    ROMLB_3_3_bytes = ROMLB_3_3_bin_read.ReadBytes(ROMLB_3_3_Bin_Size);
+                    ROMLB_3_3_bin_read.Close();
+                    fs1.Close();
+                }
+
+
 
                 ROM_Path = openFileDialog.FileName;
 
@@ -82,18 +134,28 @@ namespace Multi_IAP_Application
             openFileDialog.Multiselect = true;//允许同时选择多个文件
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-   
-                string strFileName = openFileDialog.FileName;
-                listView1.Items[1].SubItems[1].Text = Path.GetFileName(strFileName);
 
-                textBox3.Text = openFileDialog.FileName;
+                ROMLB3_4_Path = openFileDialog.FileName;
+                listView1.Items[1].SubItems[1].Text = Path.GetFileName(ROMLB3_4_Path);
 
-                Properties.Settings.Default.LB3_4_FilePath = textBox3.Text;
+                textBox3.Text = ROMLB3_4_Path;
+
+                Properties.Settings.Default.LB3_4_FilePath = ROMLB3_4_Path;
                 Properties.Settings.Default.Save();  // save 文件路径
                 string tPath = Properties.Settings.Default.LB3_4_FilePath;
-            
 
-
+                if (ROMLB3_4_Path != "")
+                {
+                    FileInfo info2 = new FileInfo(openFileDialog.FileName);
+                    int size2 = (int)info2.Length;
+                    ROMLB_3_4_Bin_Size = size2;
+                    FileStream fs2 = new FileStream(openFileDialog.FileName, FileMode.Open, FileAccess.Read);
+                    ROMLB_3_4_bin_read = new BinaryReader(fs2);
+                    ROMLB_3_4_bytes = new byte[ROMLB_3_4_Bin_Size];
+                    ROMLB_3_4_bytes = ROMLB_3_4_bin_read.ReadBytes(ROMLB_3_4_Bin_Size);
+                    ROMLB_3_4_bin_read.Close();
+                    fs2.Close();
+                }
                 ROM_Path2 = openFileDialog.FileName;
 
             }
